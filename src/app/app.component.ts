5c74e59bfd33f580ab7a882d52154254343320c1
Sheikh Forid Ahmed Shanto <Sheikh15-3700@diu.edu.bd>
2025-09-24 19:09:19 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form-task';
+
+  source:any;
+
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  gender: string = '';
+
+  onSubmit(form: NgForm) {
+    this.source = form.value;
+    console.log(form.value);
+  }
 }
